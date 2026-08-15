7484dbb0600c83f66e9cef96b53bef15bcd29e5e
ENKNOT <256907366+enknot96@users.noreply.github.com>
2026-08-16 08:32:45 +0900

    local-ocr環境構築など

---
+++ packages/local-ocr/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import manifest from "./manifest.json";
+
+export default defineConfig({
+  plugins: [
+    crx({ manifest }),
+    viteStaticCopy({
+      targets: [
+        { src: "node_modules/tesseract.js/dist/worker.min.js", dest: "." },
+        { src: "node_modules/tesseract.js-core/tesseract-core*.wasm.js", dest: "." },
+        { src: "node_modules/@tesseract.js-data/jpn/4.0.0/jpn.traineddata.gz", dest: "tessdata" },
+      ],
+    }),
+  ],
+});
